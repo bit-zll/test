3fbf3adc175cd1bb1747039f99ebda67dfd94079
bit-zll <524183259@qq.com>
2020-03-30 11:48:36 +0800

    结构体和实用调试技巧

---
+++ test_3_30/test.c
@@ -0,0 +1,180 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//typedef struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//int main()
+//{
+//	Stu s1 = { "张三", 20, "15249287076", "男" };
+//	return 0;
+//}
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* pc;
+//};
+//int main()
+//{
+//	char arr[] = "hello bit\n";
+//	struct T t = { "hehe", { 100, 'w', "hello world", 3.14 }, arr };
+//	printf("%s\n", t.ch);
+//	printf("%s\n", t.s.arr);
+//	printf("%lf\n", t.s.d);
+//	printf("%s\n", t.pc);
+//	return 0;
+//}
+
+//typedef struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//void print1(Stu tmp)
+//{
+//	printf("name:%s\n", tmp.name);
+//	printf("age:%d\n", tmp.age);
+//	printf("tele:%s\n", tmp.tele);
+//	printf("sex:%s\n", tmp.sex);
+//}
+//void print2(Stu* ps)
+//{
+//	printf("name:%s\n", ps->name);
+//	printf("age:%d\n", ps->age);
+//	printf("tele:%s\n", ps->tele);
+//	printf("sex:%s\n", ps->sex);
+//}
+//int main()
+//{
+//	Stu s = { "李四", 40, "15598886688", "男" };
+//	/*print1(s);*/
+//	print2(&s);      //print2更好一些
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//		printf("%d ", 10 - i);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d",&n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	//printf("%p\n", arr);
+//	//printf("%p\n", &i);
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n"); 
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+#include<assert.h>
+//char*my_strcpy(char*dest, const char*src)
+//{
+//	char*ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	//把src指向的字符串拷贝到dest指向的空间，包含'\0'字符
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[] = "######";
+//	char arr2[] = "bit";
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 0;
+//	int n = 10;
+//	int* p = &num;
+//	p = &n;
+//	return 0;
+//}
+//int my_strlen(const char*str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
