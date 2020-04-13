c6c1a4d59544f39008a59e36edf6e0091bdf4c92
bit-zll <524183259@qq.com>
2020-04-13 21:26:17 +0800

    提交代码

---
+++ test_4_13/test.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//最大公约数
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	while ((a - b) != 0)
+//	{
+//		if (a > b)
+//		{
+//			a = a - b;
+//		}
+//		else
+//		{
+//			b = b - a;
+//		}
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+//最小公倍数1
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a,&b);
+//	int c = a*b;
+//	while ((a - b) != 0)
+//	{
+//		if (a > b)
+//		{
+//			a = a - b;
+//		}
+//		else
+//		{
+//			b = b - a;
+//		}
+//	}
+//	c = c / a;
+//	printf("%d\n", c);
+//	return 0;
+//}
+//最小公倍数2
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	int i = 1;
+//	while ((a*i) % b != 0)
+//	{
+//		i++;
+//	}
+//	printf("%d\n", a*i);
+//	return 0;
+//}
