66ea64cb3764ba11eae508e5fe802bf91375df5d
bit-zll <524183259@qq.com>
2020-04-16 18:57:22 +0800

    作业代码

---
+++ test_4_16/test.c
@@ -0,0 +1,229 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int main()
+//{
+//	unsigned int a = 0x1234;
+//	unsigned char b = *(unsigned char *)&a;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//杨辉三角
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 10; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else
+//			{
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//		}
+//
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//题目名称：
+//猜凶手
+//题目内容：
+//日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为4个嫌疑犯的一个。
+//
+//以下为4个嫌疑犯的供词 :
+//A说：不是我。//0
+//B说：是C。   //0
+//C说：是D。   //0
+//D说：C在胡说 //1
+//已知3个人说了真话，1个人说的是假话。
+//
+//现在请根据这些信息，写一个程序来确定到底谁是凶手
+
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("凶手是：%c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b == 2) + (a == 3)) == 1 &&
+//							((b == 2) + (e == 4)) == 1 &&
+//							((c == 1) + (d == 2)) == 1 &&
+//							((c == 5) + (d == 3)) == 1 &&
+//							((e == 4) + (a == 1)) == 1)
+//						{
+//							if (a*b*c*d*e == 120)
+//							{
+//								printf("a=%d,b=%d,c=%d,d=%d,e=%d", a, b, c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a[5] = { 5, 4, 3, 2, 1 };
+//	//5 4 3 2 1 
+//	int *ptr = (int *)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));
+//	return 0;
+//}
+//int main()
+//{
+//	int aa[2][5] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//	int *ptr1 = (int *)(&aa + 1);
+//	int *ptr2 = (int *)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//
+//	return 0;
+//}
+
+//题目内容：
+//实现一个函数，可以左旋字符串中的k个字符。
+//例如：
+//ABCD左旋一个字符得到BCDA
+//ABCD左旋两个字符得到CDAB
+
+#include <string.h>
+#include <assert.h>
+//第一种方法
+void left_move(char* arr, int k)
+{
+	assert(arr != NULL);
+	int len = strlen(arr);
+	int i = 0;
+	for (i = 0; i < k; i++)
+	{
+		char tmp = *arr;
+		int j = 0;
+		for (j = 0; j < len-1; j++)
+		{
+			*(arr + j) = *(arr + j + 1);
+		}
+		*(arr + len - 1) = tmp;
+	}
+}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	left_move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//第二种
+//abcdef
+//bafedc
+//cdefab
+//void reverse(char* left, char* right)
+//{
+//	assert(left != NULL);
+//	assert(right != NULL);
+//	while (left < right)
+//	{
+//		int tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//void left_move(char* arr, int k)
+//{
+//	assert(arr != NULL);
+//	int len = strlen(arr);
+//	assert(k <= len);
+//	reverse(arr, arr + k - 1);
+//	reverse(arr + k, arr + len - 1);
+//	reverse(arr, arr + len - 1);
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	left_move(arr, k);
+//	printf("%s", arr);
+//	return 0;
+//}
+
+int is_left_move(char* s1, char* s2)
+{
+	int len = strlen(s1);
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		left_move(s1, 1);
+		int ret = strcmp(s1, s2);
+		if (ret == 0)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret = is_left_move(arr1, arr2);
+	if (ret == 1)
+	{
+		printf("yes\n");
+	}
+	else
+	{
+		printf("no\n");
+	}
+	return 0;
+}
