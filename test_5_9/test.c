2721612bc441d01f671f5ff725fde705311d17e3
bit-zll <524183259@qq.com>
2020-05-09 11:32:31 +0800

    文件操作

---
+++ test_5_9/test.c
@@ -0,0 +1,267 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	//写文件
+//	fputc('b', pf);
+//	fputc('i', pf);
+//	fputc('t', pf);
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt","r");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	int ch = fgetc(pf);
+//	printf("%c", ch);
+//	ch = fgetc(pf);
+//	printf("%c", ch);
+//	ch = fgetc(pf);
+//	printf("%c", ch);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	char buf[1024] = { 0 };
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//读文件
+//	fgets(buf, 1024, pf);
+//	printf("%s", buf);
+//	fgets(buf, 1024, pf);
+//	printf("%s", buf);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	char buf[1024] = { 0 };
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//写文件
+//	fputs("hello\n", pf);
+//	fputs("world\n", pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//int main()
+//{
+//	struct S s = { 100, 3.14f, "bit" };
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//格式化的形式写文件
+//	fprintf(pf, "%d %.2f %s", s.n, s.score, s.arr);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//格式化的输入数据
+//	fscanf(pf, "%d %f %s", &(s.n), &(s.score), s.arr);
+//	printf("%d %.2f %s\n", s.n, s.score, s.arr);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	
+//	
+//	fscanf(stdin, "%d %f %s", &(s.n), &(s.score), s.arr);
+//	fprintf(stdout,"%d %.2f %s\n", s.n, s.score, s.arr);
+//	
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//int main()
+//{
+//	struct S s = { 100, 3.14f, "abcdef" };
+//	struct S tmp = { 0 };
+//	char buf[1024] = { 0 };
+//	//把格式化的数据转换成字符串存储到buf
+//	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
+//	//printf("%s\n", buf);
+//	//从buf中读取格式化的数据到tmp中
+//	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
+//	printf("%d %f %s\n", tmp.n, tmp.score, tmp.arr);
+//  return 0;
+//}
+
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//int main()
+//{
+//	struct S s = { "张三", 20, 55.6 };
+//	FILE* pf = fopen("test.txt", "wb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//二进制的形式写文件
+//	fwrite(&s, sizeof(struct S), 1, pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//int main()
+//{
+//	struct S tmp = { 0 };
+//	FILE* pf = fopen("test.txt", "rb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//二进制的形式读文件
+//	fread(&tmp, sizeof(struct S), 1, pf);
+//	printf("%s %d %lf\n", tmp.name, tmp.age, tmp.score);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//1.定位文件指针
+//	fseek(pf, -2, SEEK_END);
+//	//2.读取文件
+//	int ch = fgetc(pf);
+//	//printf("%c\n", ch);
+//
+//	//相对起始位置的偏移量
+//	//int pos = ftell(pf);
+//	//printf("%d\n", pos);
+//
+//	//回到起始位置
+//	rewind(pf);
+//	ch = fgetc(pf);
+//	printf("%c\n", ch);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test2.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("open file text2.txt");
+//		return 0;
+//	}
+//	//读文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+int main()
+{
+	FILE* pf = fopen("test.txt", "r");
+	if (pf == NULL)
+	{
+		perror("open file test.txt");
+		return 0;
+	}
+	int ch = 0;
+	while ((ch = fgetc(pf)) != EOF)
+	{
+		putchar(ch);
+	}
+	if (ferror(pf))
+	{
+		printf("error\n");
+	}
+	else if (feof(pf))
+	{
+		printf("end of file\n");
+	}
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
